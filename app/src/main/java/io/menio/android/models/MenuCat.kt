ff1fbe30afea9fa3f76d0474f40f85a6520affec
Amin <aminkeshavarzian@gmail.com>
2017-11-02 16:52:59 +0330

    init 4

---
+++ app/src/main/java/io/menio/android/models/MenuCat.kt
@@ -0,0 +1,13 @@
+package io.menio.android.models
+
+/**
+ * Created by Amin on 02/11/2017.
+ */
+class MenuCat{
+    var title: String = ""
+    public var price: Int = 0
+    var id: Int = 0
+    var desc: String = ""
+
+
+}
